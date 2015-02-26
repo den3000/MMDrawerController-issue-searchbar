0edc086c748ec8a67cdcf30157ec0d870f9ae69e
Suprun Denis <suprundenis@gmail.com>
2015-02-26 07:56:16 +0300

    Now everything OK and fix could be counted as sufficient.

--- MMDrawerController-issue-searchbar/ViewController.m
+++ MMDrawerController-issue-searchbar/ViewController.m
@@ -19,6 +19,7 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     self.navigationController.navigationBar.translucent = NO;
+    self.extendedLayoutIncludesOpaqueBars = YES;
 }
 
 - (void)didReceiveMemoryWarning {
