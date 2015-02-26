81ed76779ee825f13ffcfed9e68c2cdbfbe6d2e4
Suprun Denis <suprundenis@gmail.com>
2015-02-26 07:39:29 +0300

    Starting example 04

--- MMDrawerController-issue-searchbar/ViewController.h
+++ MMDrawerController-issue-searchbar/ViewController.h
@@ -9,7 +9,9 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UITableViewController
-
+<UISearchResultsUpdating, UISearchControllerDelegate, UISearchBarDelegate>{
+    UISearchController * mSearchController;
+}
 
 @end
 
