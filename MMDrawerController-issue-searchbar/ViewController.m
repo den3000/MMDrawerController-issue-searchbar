063a33680a1d6eb3e46ce782f4c02d6a72ea251a
Suprun Denis <suprundenis@gmail.com>
2015-02-26 08:01:40 +0300

    This could be counted as sufficient fix.

--- MMDrawerController-issue-searchbar/ViewController.m
+++ MMDrawerController-issue-searchbar/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    self.navigationController.navigationBar.translucent = NO;
+    self.extendedLayoutIncludesOpaqueBars = YES;
+    
     [self initSearch];
 }
 
