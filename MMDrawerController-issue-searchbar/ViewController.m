b020d6b54a2ea077fb7689131e60f580c20c1c81
Suprun Denis <suprundenis@gmail.com>
2015-02-26 07:26:39 +0300

    Minor fixes

--- MMDrawerController-issue-searchbar/ViewController.m
+++ MMDrawerController-issue-searchbar/ViewController.m
@@ -17,16 +17,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    
-//    self.navigationController.navigationBar.translucent = NO;
-//    self.navigationController.extendedLayoutIncludesOpaqueBars = NO;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
-    
-
 }
 
 @end
