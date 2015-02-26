f98e4f5d0bbac9b996ceaebb2083c02cc450d448
Suprun Denis <suprundenis@gmail.com>
2015-02-26 07:30:49 +0300

    Possible fix for example 03. But still, this fix couldn't be counted as sufficient as now search bar navigation is not flawless enough.

--- MMDrawerController-issue-searchbar/ViewController.m
+++ MMDrawerController-issue-searchbar/ViewController.m
@@ -17,6 +17,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.navigationController.navigationBar.translucent = NO;
 }
 
 - (void)didReceiveMemoryWarning {
