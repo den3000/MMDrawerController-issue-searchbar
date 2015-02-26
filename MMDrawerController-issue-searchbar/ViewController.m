cef66ffecb4ae01843c8a5889b75ac241df06c39
Suprun Denis <suprundenis@gmail.com>
2015-02-26 07:45:45 +0300

    Here comes the main problem for rightSideController - UISearchBar jumps to right on pressing

--- MMDrawerController-issue-searchbar/ViewController.m
+++ MMDrawerController-issue-searchbar/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+-(void) initSearch;
 @end
 
 @implementation ViewController
@@ -17,11 +17,32 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self initSearch];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)initSearch{
+    mSearchController = [[UISearchController alloc] initWithSearchResultsController:nil];
+    mSearchController.searchResultsUpdater = self;
+    [mSearchController.searchBar sizeToFit];
+    mSearchController.delegate = self;
+    mSearchController.dimsBackgroundDuringPresentation = NO; // default is YES
+    mSearchController.searchBar.delegate = self; // so we can monitor text changes + others
+    
+    self.tableView.tableHeaderView = mSearchController.searchBar;
+//   	self.definesPresentationContext = YES;  // know where you want UISearchController to be displayed
+    mSearchController.definesPresentationContext = YES;
+}
+
+#pragma mark - UISearchControllerDelegate
+
+-(void)updateSearchResultsForSearchController:(UISearchController *)searchController{
+    
 }
 
 @end
